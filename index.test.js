5a8b8d7f0ff7d59abc311911c4c666e366ac9b1b
Dinesh Babu <emailerdinesh@gmail.com>
2023-02-22 11:54:54 +0530

    refactor function to optimise logic

--- index.test.js
+++ index.test.js
@@ -17,11 +17,11 @@ describe("deterministicPartitionKey", () => {
 
   it("should hash partition key when its greater than 256 char", () => {
     const event = {
-      // string with 257 `undefined,undefined...undefined`
-      partitionKey: [].fill(257).join(""),
+      // string with 257 `000...000`
+      partitionKey: new Array(257).fill(0).join(" "),
     };
     const hashOf257Undefined =
-      "b7478342a465088fc33d43a64cd370737e5a3bf6749ca62c1d6db341beb987326b4df3a9f54f67a2f0ee915d4216af2f382fda14dd58dc67794f745e92d7a7f6";
+      "ea55bb6406e48bfdc61c0a440e59bc5c5921de1811512f6125960036426a7481b125495bf6c280194cc6c0f401cf78f40b6da3408a550023e71da2976fa2ca15";
     expect(deterministicPartitionKey(event)).toBe(hashOf257Undefined);
   });
 
@@ -32,5 +32,9 @@ describe("deterministicPartitionKey", () => {
     const expectedHash =
       "53f109aeb096166efd118847185e29185538d9dfd34c146e1b566d30100cf08e1324b5a6197f07451a47f8cda25e578206dd778cab1f1582412c9f08fd5c2e57";
     expect(deterministicPartitionKey(event)).toBe(expectedHash);
+  });
+
+  it("should return TRIVIAL_PARTITION_KEY when event is undefined", () => {
+    expect(deterministicPartitionKey()).toBe("0");
   });
 });
