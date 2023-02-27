cc297a37b0f3652060e07862c157f6c642827900
Dinesh Babu <emailerdinesh@gmail.com>
2023-02-27 13:21:49 +0530

    mock crypto to test code independant of imports

--- index.test.js
+++ index.test.js
@@ -1,5 +1,15 @@
 const { deterministicPartitionKey } = require("./index");
 
+/**
+ * The mock helps us test our code independant of the imports
+ */
+jest.mock("crypto", () => ({
+  createHash: () => ({
+    update: jest.fn().mockReturnThis(),
+    digest: jest.fn().mockReturnValue("encrypt 123"),
+  }),
+}));
+
 describe("deterministicPartitionKey", () => {
   it("should use partition key when it exists", () => {
     const event = {
@@ -20,18 +30,16 @@ describe("deterministicPartitionKey", () => {
       // string with 257 `000...000`
       partitionKey: new Array(257).fill(0).join(" "),
     };
-    const hashOf257Undefined =
-      "ea55bb6406e48bfdc61c0a440e59bc5c5921de1811512f6125960036426a7481b125495bf6c280194cc6c0f401cf78f40b6da3408a550023e71da2976fa2ca15";
-    expect(deterministicPartitionKey(event)).toBe(hashOf257Undefined);
+
+    expect(deterministicPartitionKey(event)).toBe("encrypt 123");
   });
 
   it("should hash entire event when there is no partition key", () => {
     const event = {
       name: "dinesh",
     };
-    const expectedHash =
-      "53f109aeb096166efd118847185e29185538d9dfd34c146e1b566d30100cf08e1324b5a6197f07451a47f8cda25e578206dd778cab1f1582412c9f08fd5c2e57";
-    expect(deterministicPartitionKey(event)).toBe(expectedHash);
+
+    expect(deterministicPartitionKey(event)).toBe("encrypt 123");
   });
 
   it("should return TRIVIAL_PARTITION_KEY when event is undefined", () => {
